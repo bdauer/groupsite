a898b3e626d443f3d4434e74bdca69c5dacef70a
bdauer <dauer.benjamin@gmail.com>
2017-03-11 16:35:29 -0500

    Add managers and add a users many-to-many field in the Group model.

--- groupsite/models.py
+++ groupsite/models.py
@@ -4,16 +4,39 @@ from django.db import models
 
 # Create your models here.
 
+class GroupManager(models.Manager):
+
+    def get_user_groups(self, user):
+        """
+        Return all groups that have the user as a member.
+        """
+        pass
 
 class Group(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
     creator = models.OneToOneField(User)
+    users = models.ManyToManyField(User)
 
 
 class User_Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField()
+
+class InvitationManager(models.Manager):
+
+    def get_pending_sent_invites(self, user):
+        """
+        Return all pending sent invites.
+        """
+        pass
+
+    def get_pending_received_invites(self, user):
+        """
+        Return all pending received invites.
+        """
+        pass
+
 
 class Invitation(models.Model):
     group = models.OneToOneField(Group)
