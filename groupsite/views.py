bdbcf1a65a5b3d8a809943a634fd807a04777dec
bdauer <dauer.benjamin@gmail.com>
2017-03-11 16:56:50 -0500

    Add notes for designing views

--- groupsite/views.py
+++ groupsite/views.py
@@ -3,3 +3,22 @@ from django.http import (HttpResponse, HttpRequest)
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+# Profile
+#   name, avatar, bio.
+#
+# Edit Profile
+#   name, avatar, bio.
+
+# Groups
+# My groups
+#   name, description. Click to edit.
+# Pending invites
+# name, description, inviter, accept, decline.
+
+# Create Group
+#   name
+#   description
+
+# Manage Group (only my groups)
+#   name, dscription, invite, current users
