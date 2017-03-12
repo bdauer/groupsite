d9bfc0ce8752f6ad0faa247a9fb0683aeafe60b2
bdauer <dauer.benjamin@gmail.com>
2017-03-12 15:37:54 -0400

    Add a GroupsView

--- groupsite/views.py
+++ groupsite/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 from django.http import (HttpResponse, HttpRequest, HttpResponseRedirect)
-from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import UserGroup, Invitation
+
 # Create your views here.
 def index(request):
     return render(request, 'groupsite/index.html')
@@ -9,6 +12,23 @@ def index(request):
 def login(request):
     return render(request, 'groupsite/login.html')
 
+
+class GroupsView(LoginRequiredMixin, generic.ListView):
+    """
+    Show all of a user's groups and invitations.
+    """
+    template_name = "groupsite/groups.html"
+    model = UserGroup
+
+    def get_context_data(self, **kwargs):
+        data = super(generic.ListView, self).get_context_data(**kwargs)
+        request = self.request
+        data['groups'] = UserGroup.objects.get_user_groups(request.user)
+        data['sent_invites'] = Invitation.objects.get_pending_sent_invites(
+                                                                    request.user)
+        data['received_invites'] = Invitation.objects.get_pending_received_invites(
+                                                                        request.user)
+        return data
 
 # Profile
 #   name, avatar, bio.
