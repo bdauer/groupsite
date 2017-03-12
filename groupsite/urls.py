57fe06878184fd018ce9ae93562516b2d445b84a
bdauer <dauer.benjamin@gmail.com>
2017-03-12 16:04:52 -0400

    Add creategroup url

--- groupsite/urls.py
+++ groupsite/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login$', views.login, name='login'),
     url(r'^groups$', views.GroupsView.as_view(), name='groups'),
+    url(r'^creategroup$', views.CreateGroupView.as_view(), name='create group')
     ]
