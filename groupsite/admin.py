00896ea3215dce8a105030c969cd033ff5daac82
bdauer <dauer.benjamin@gmail.com>
2017-03-12 15:35:31 -0400

    Add admin.py and add models to the admin panel

--- groupsite/admin.py
+++ groupsite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Invitation, UserGroup, UserProfile
+
+admin.site.register(Invitation)
+admin.site.register(UserGroup)
+admin.site.register(UserProfile)
