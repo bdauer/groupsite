86e352b8677ad4c1c7617cdef039943eed7ff3b8
bdauer <dauer.benjamin@gmail.com>
2017-03-13 10:27:45 -0400

    Remove unnecessary code

--- groupsite/pipeline.py
+++ groupsite/pipeline.py
@@ -10,13 +10,4 @@ def save_user(backend, user, response, *args, **kwargs):
         profile = user.userprofile
     except ObjectDoesNotExist:
         profile = UserProfile(user_id=user.id)
-    if backend.name == 'facebook':
-        profile.facebook_id = response.get('id')
-    if backend.name == 'google-oauth2':
-        try:
-            profile.google_id = response.get('id')
-            profile.save()
-        except OverflowError:
-            profile.google_id = response.get('id')[0:15]
-
     return {'profile': profile}
