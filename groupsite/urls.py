157ac97cd89c438e6b2a1c32212fcca697f3c6d7
bdauer <dauer.benjamin@gmail.com>
2017-03-12 21:38:40 -0400

    Add urls for update group and group detail

--- groupsite/urls.py
+++ groupsite/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login$', views.login, name='login'),
     url(r'^groups$', views.GroupsView.as_view(), name='groups'),
-    url(r'^creategroup$', views.CreateGroupView.as_view(), name='create group')
+    url(r'^creategroup$', views.CreateGroupView.as_view(), name='create group'),
+    url(r'^updategroup/(?P<pk>[0-9]+)/$', views.ManageGroupView.as_view(), name='update group'),
+    url(r'^groupdetails/(?P<pk>[0-9]+)/$', views.GroupDetailView.as_view(), name='group detail')
     ]
