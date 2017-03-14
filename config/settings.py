84fa8af22af0f0414989f78e07346f60a612c390
bdauer <dauer.benjamin@gmail.com>
2017-03-14 17:40:57 -0400

    add commented out fb auth backend in case needed, but wasn't documented.

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
+    # 'social_core.backends.facebook.FacebookOAuth2',
      'django.contrib.auth.backends.ModelBackend'
 )
 
