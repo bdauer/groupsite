c75c246fc57a47aaf489b879793e54cade2594da
bdauer <dauer.benjamin@gmail.com>
2017-03-14 15:42:50 -0400

    Update with ajax

--- groupsite/static/groupsite/invites.js
+++ groupsite/static/groupsite/invites.js
@@ -18,17 +18,21 @@ function addInviteResponseListeners(buttons) {
         // allowing for every button to get a listener.
       (function(button) {
         button.addEventListener('click', function() {
+            updateInviteAjaxRequest(button);
             if (button.name.startsWith("accept")) {
                 updateGroups(button);
                 }
         var parent = button.parentNode.parentNode;
         parent.parentNode.remove(parent);
-        // updateInviteAjaxRequest(button);
+
+
         });
       })(button);
     }
 }
-
+/**
+Update groups after an invite is accepted.
+*/
 function updateGroups(button) {
     var inviteParent = button.parentNode.parentNode;
     var groupId = inviteParent.action.split('/')[4];
@@ -49,5 +53,51 @@ function updateGroups(button) {
 
     newGroupForm.appendChild(newGroupButton);
     groupsList.appendChild(newGroupForm);
+}
 
+function updateInviteAjaxRequest(button) {
+    var vals = button.name.split('_')
+    var newStatus = vals[0];
+    var inviteId = vals[1];
+
+    $.ajaxSetup({
+      beforeSend: function(xhr, settings) {
+          if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+              xhr.setRequestHeader("X-CSRFToken", csrftoken);
+          }
+      }
+  });
+    var csrftoken = getCookieValue('csrftoken');
+    $.ajax({
+        url: 'updateinvite/',
+        type: 'POST',
+        data: {
+            'invite_id': inviteId,
+            'new_status': newStatus
+        }
+    })
+};
+
+function getCookieValue(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      console.log(cookies)
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
+
+
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
